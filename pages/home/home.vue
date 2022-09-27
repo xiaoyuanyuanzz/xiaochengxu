c4492d4e732b3fdc7175913ee14dbf65413751d3
xiaoyuanyuanzzz <1015944668@qq.com>
2022-09-27 17:23:03 +0800

    完成了home首页的开发

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -10,8 +10,31 @@
 		
 		<!-- 分类导航区域 -->
 		<view class="nav-list">
-			<view class="nav-item" v-for="(item,i) in navList" :key="i">
+			<view class="nav-item" v-for="(item,i) in navList" :key="i" @click="nav_handler(item)">
 				<image :src="item.image_src" class="nav-image"></image>
+			</view>
+		</view>
+		
+		<!-- 楼层区域 -->
+		<view class="floor-list">
+			<!-- 楼层Item项 -->
+			<view class="floor-item" v-for="(item,i) in floorList" :key="i">
+				<!-- 楼层标题 -->
+				<image :src="item.floor_title.image_src" class="floor-title"></image>
+				<!-- 楼层图片区域 -->
+				<view class="floor-img-box">
+					<!-- 左侧 -->
+					<navigator class="left-img-box" :url="item.product_list[0].url">
+						<image :src="item.product_list[0].image_src" :style="'width:'+item.product_list[0].image_width+'rpx'" mode="widthFix"></image>
+					</navigator>
+					<!-- 右侧 -->
+					<navigator class="right-img-box">
+						<view class="right-img-item" v-for="(item2,i2) in item.product_list" :key="i2" v-if="i2 !== 0">
+							<image :src="item2.image_src" mode="widthFix" :style="'width:'+item2.image_width + 'rpx'"></image>
+						</view>
+					</navigator>
+
+				</view>
 			</view>
 		</view>
 	</view>
@@ -24,10 +47,18 @@
 				//轮播图的数据列表，默认为空
 				swiperList:[],
 				//分类导航的数据列表
-				navList:[]
+				navList:[],
+				floorList:[]
 			};
 		},
 		methods:{
+			nav_handler(item){
+				if(item.name === '分类'){
+					uni.switchTab({
+						url:'/pages/cate/cate'
+					})
+				}
+			},
 			async getSwiperList(){
 				const {data:res} = await uni.$http.get('/api/public/v1/home/swiperdata')
 				if(res.meta.status !== 200){
@@ -42,13 +73,28 @@
 					return uni.$showMsg()
 				}
 				this.navList = res.message
-				console.log(this.navList)
+			},
+			async getFloorList(){
+				const {data:res} = await uni.$http.get('/api/public/v1/home/floordata')
+				if(res.meta.status !== 200){
+					return uni.$showMsg()
+				}
+				//处理url地址，替换为自己的路径
+				res.message.forEach(floor => {
+					floor.product_list.forEach(prod => {
+						prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+					})
+				})
+				this.floorList = res.message
+				console.log(this.floorList)
 			}
+
 		},
 		onLoad(){
 			//在小程序页面刚加载时，调用获取轮播图数据的方法
 			this.getSwiperList()
 			this.getNavList()
+			this.getFloorList()
 		}
 	}
 </script>
@@ -70,5 +116,19 @@
 			width: 128rpx;
 			height: 140rpx;
 		}
+	}
+	.floor-title{
+		height: 60rpx;
+		width: 100%;
+		display: flex;
+	}
+	.floor-img-box{
+		display: flex;
+		padding-left: 10rpx;
+	}
+	.right-img-box{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
 	}
 </style>
